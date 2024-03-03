5d13a3153c113bd99f67ace2e99f5ace51554284
Ninn04 <lynin722@gmail.com>
2024-03-03 13:21:01 +0700

    update

--- src/components/hero/Ellipse.tsx
+++ src/components/hero/Ellipse.tsx
@@ -1,16 +1,11 @@
 import React, { useEffect } from 'react'
-// import CircleType from `circletype`;
+
 
 const Ellipse = () => {
-  // useEffect(() => {
-  //   const circleOfSkills = new CircleType(document.getElementById('rounded-text'))
-  //   .radius(100)
-  //   return circleOfSkills.mount()
-  // }, []);
 
   return (
     <div className='cirlce'>
-        SCROLLDOWN SCROLLDOWN
+        <p style={{"transfor"}}>SCROLLDOWN SCROLLDOWN</p>
     </div>
   )
 }
